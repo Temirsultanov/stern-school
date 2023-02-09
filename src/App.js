669ea5ef4a59be0a1c5c4887e4bb24d7bb59c944
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2023-02-09 18:33:22 +0300

    home page animations

--- src/App.js
+++ src/App.js
@@ -6,16 +6,45 @@ import Header from './components/Header/Header'
 import Offer from './components/Offer/Offer'
 import 'swiper/css'
 import Teachers from './components/Teachers/Teachers'
+import { motion } from 'framer-motion'
+
+const components = [<Teachers />, <Attributes />, <Comments />]
+
+const cardVariants = {
+    offscreen: {
+        y: 150,
+    },
+    onscreen: {
+        y: 0,
+        transition: {
+            type: 'spring',
+            bounce: 0.2,
+            duration: 0.5,
+        },
+    },
+}
+
+function Card({ emoji, hueA, hueB }) {
+    return (
+        <motion.div
+            className="card-container"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.2 }}
+        >
+            <motion.div variants={cardVariants}>{emoji}</motion.div>
+        </motion.div>
+    )
+}
 
 const App = () => {
     return (
         <>
-            <Header />
-            <Offer />
-            <About />
-            <Teachers />
-            <Attributes />
-            <Comments />
+            <Header /> 
+            <Offer /><About />
+            {components.map((item) => {
+                return <Card emoji={item} />
+            })}
         </>
     )
 }
