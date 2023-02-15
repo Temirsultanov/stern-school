4a080ef616326e6abb6d56de81831d371d4d2dde
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2023-02-15 19:11:04 +0300

    fixes

--- src/components/Cost/Cost.jsx
+++ src/components/Cost/Cost.jsx
@@ -128,83 +128,73 @@ const Cost = ({ setShow }) => {
                         </Swiper>
                     </div>
                     <div className="adaptive_slider">
-                        <Swiper
-                            loop={true}
-                            spaceBetween={20}
-                            slidesPerView={1}
-                            initialSlide={1}
-                        >
-                            {[1, 3, 42].map((item, index) => (
-                                <SwiperSlide className="adaptive_card">
-                                    <h4>Стоимость</h4>
-                                    <div className="slides">
-                                        <Swiper
-                                            spaceBetween={10}
-                                            slidesPerView={1.4}
-                                            className="mySwiper"
-                                        >
-                                            <SwiperSlide
-                                                onClick={() => setGrade(0)}
-                                                className={
-                                                    grade === 0
-                                                        ? 'slide active'
-                                                        : 'slide'
-                                                }
-                                            >
-                                                1-8 класс
-                                            </SwiperSlide>
-                                            <SwiperSlide
-                                                onClick={() => setGrade(1)}
-                                                className={
-                                                    grade === 1
-                                                        ? 'slide active'
-                                                        : 'slide'
-                                                }
-                                            >
-                                                10-11 класс
-                                            </SwiperSlide>
-                                        </Swiper>
+                        <div className="adaptive_card">
+                            <h4>Стоимость</h4>
+                            <div className="slides">
+                                <Swiper
+                                    spaceBetween={10}
+                                    slidesPerView={1.4}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide
+                                        onClick={() => setGrade(0)}
+                                        className={
+                                            grade === 0
+                                                ? 'slide active'
+                                                : 'slide'
+                                        }
+                                    >
+                                        1-8 класс
+                                    </SwiperSlide>
+                                    <SwiperSlide
+                                        onClick={() => setGrade(1)}
+                                        className={
+                                            grade === 1
+                                                ? 'slide active'
+                                                : 'slide'
+                                        }
+                                    >
+                                        10-11 класс
+                                    </SwiperSlide>
+                                </Swiper>
+                            </div>
+                            <div className="cost_block">44 900 ₽</div>
+                            <p className="desc">
+                                <span>60 000 ₽</span> - стоимость такого же
+                                количества занятий в школе
+                            </p>
+                            <div className="yellow_block">
+                                60 000₽ - 44 900₽ = 15 100₽ экономии
+                            </div>
+                            <div className="cards_adaptive">
+                                <div className="adaptive_card2">
+                                    <div className="card_image">
+                                        <img
+                                            src="./images/cost/roket.png"
+                                            alt=""
+                                            className="roket"
+                                        />
                                     </div>
-                                    <div className="cost_block">44 900 ₽</div>
-                                    <p className="desc">
-                                        <span>60 000 ₽</span> - стоимость такого
-                                        же количества занятий в школе
+                                    <p>
+                                        Эффект 3 месяцев занятий всего за 8 дней
                                     </p>
-                                    <div className="yellow_block">
-                                        60 000₽ - 44 900₽ = 15 100₽ экономии
+                                </div>
+                                <div className="adaptive_card2">
+                                    <div className="card_image">
+                                        <img
+                                            src="./images/cost/moun2.png"
+                                            alt=""
+                                            className="moun"
+                                        />
                                     </div>
-                                    <div className="cards_adaptive">
-                                        <div className="adaptive_card2">
-                                            <div className="card_image">
-                                                <img
-                                                    src="./images/cost/roket.png"
-                                                    alt=""
-                                                    className="roket"
-                                                />
-                                            </div>
-                                            <p>
-                                                Эффект 3 месяцев занятий всего
-                                                за 8 дней
-                                            </p>
-                                        </div>
-                                        <div className="adaptive_card2">
-                                            <div className="card_image">
-                                                <img
-                                                    src="./images/cost/moun2.png"
-                                                    alt=""
-                                                    className="moun"
-                                                />
-                                            </div>
-                                            <p>
-                                                Самый лучший способ заполнить
-                                                все пробелы в знаних и
-                                                подготовиться к ОГЭ/ЕГЭ
-                                            </p>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
+                                    <p>
+                                        Самый лучший способ заполнить все
+                                        пробелы в знаних и подготовиться к
+                                        ОГЭ/ЕГЭ
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <button onClick={() => setShow(true)}>Регистрация</button>
